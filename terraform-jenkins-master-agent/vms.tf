eb2c17241fea00bd18aa3af153fc4e376a4da9c5
azat m <azat.mja@gmail.com>
2022-01-23 00:12:50 -0600

    ran terraform fmt

--- terraform-jenkins-master-agent/vms.tf
+++ terraform-jenkins-master-agent/vms.tf
@@ -5,14 +5,14 @@ resource "aws_key_pair" "pub_key" {
 
 
 resource "aws_instance" "master" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.pub_key.key_name
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.allow_login.id]
-  associate_public_ip_address = "true" 
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.pub_key.key_name
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  vpc_security_group_ids      = [aws_security_group.allow_login.id]
+  associate_public_ip_address = "true"
   # depends_on = [ aws_instance.agent ] 
-  user_data = file("userdata.sh")
+  user_data  = file("userdata.sh")
   monitoring = "false"
   tags = {
     Name = "jenkins-master"
@@ -20,53 +20,53 @@ resource "aws_instance" "master" {
 }
 
 resource "aws_instance" "agent" {
-  ami                    = var.ami
-  instance_type          =  var.instance_type
-  key_name               = aws_key_pair.pub_key.key_name
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.allow_login.id]
-  associate_public_ip_address = "true" 
-  user_data = file("userdata.sh")
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.pub_key.key_name
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  vpc_security_group_ids      = [aws_security_group.allow_login.id]
+  associate_public_ip_address = "true"
+  user_data                   = file("userdata.sh")
   tags = {
     Name = "jenkins-agent"
   }
 }
 
-resource "null_resource" "master" { 
-  depends_on = [ aws_instance.master ]  
-    # triggers = { 
-    #   always_run = "${timestamp()}" 
-    # } 
-    provisioner "file" { 
-      connection { 
-        type = "ssh" 
-        user = "ubuntu" 
-        private_key = "${file("~/.ssh/id_rsa")}" 
-        host = "${aws_instance.master.public_ip}" 
-      } 
-
-      source = "master.sh" 
-      destination = "/tmp/master.sh"
+resource "null_resource" "master" {
+  depends_on = [aws_instance.master]
+  # triggers = { 
+  #   always_run = "${timestamp()}" 
+  # } 
+  provisioner "file" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = aws_instance.master.public_ip
     }
+
+    source      = "master.sh"
+    destination = "/tmp/master.sh"
+  }
 }
 
-resource "null_resource" "agent" { 
-  depends_on = [ aws_instance.agent ]  
-    # triggers = { 
-    #   always_run = "${timestamp()}" 
-    # } 
-    provisioner "file" { 
-      connection { 
-        type = "ssh" 
-        user = "ubuntu" 
-        private_key = "${file("~/.ssh/id_rsa")}" 
-        host = "${aws_instance.agent.public_ip}" 
-      } 
+resource "null_resource" "agent" {
+  depends_on = [aws_instance.agent]
+  # triggers = { 
+  #   always_run = "${timestamp()}" 
+  # } 
+  provisioner "file" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = aws_instance.agent.public_ip
+    }
 
-      source = "agent.sh" 
-      destination = "/tmp/agent.sh"     
-    } 
-} 
+    source      = "agent.sh"
+    destination = "/tmp/agent.sh"
+  }
+}
 
 output "jenkins_master_public_ip" {
   value = aws_instance.master.public_ip
