bd1f460b0a9cc2f05c9e68e96f7f9288d264585c
azat m <azat.mja@gmail.com>
2022-01-22 18:12:54 -0600

    docker start issue

--- terraform-jenkins-master-agent/master.sh
+++ terraform-jenkins-master-agent/master.sh
@@ -2,8 +2,17 @@
 
 DIR="/home/ubuntu/jenkins-in-docker-aws"
 SSH="/home/ubuntu/.ssh"
+dockerState=1
 
 cd
+while [ ! $dockerState -eq 0 ];
+do 
+sleep 2 
+echo " Waiting for docker to start ... "
+docker ps > /dev/null
+dockerState="$?"
+echo $dockerState
+done
 docker run -d --name myjenkins -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts-jdk11
 sleep 10
 cd 
