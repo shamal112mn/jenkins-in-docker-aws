4f5d0df3ebfde1dd39175762c067582ef14c8c09
azat m <azat.mja@gmail.com>
2022-01-22 18:44:22 -0600

    dependency was changed

--- terraform-jenkins-master-agent/vms.tf
+++ terraform-jenkins-master-agent/vms.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "master" {
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [aws_security_group.allow_login.id]
   associate_public_ip_address = "true" 
-  depends_on = [ aws_instance.agent ] 
+  # depends_on = [ aws_instance.agent ] 
   user_data = file("userdata.sh")
   monitoring = "false"
   tags = {
