b423bdf77bb403ab5733039ae15c936c62d59445
azat m <azat.mja@gmail.com>
2022-01-22 18:26:43 -0600

    fixing docker issue

--- terraform-jenkins-master-agent/userdata.sh
+++ terraform-jenkins-master-agent/userdata.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+dockerState=1
 
 apt-get update
 apt install docker.io -y 
+while [ ! $dockerState -eq 0 ];
+do 
+sleep 5 
+echo " USERDATA: Waiting for docker to start ... "
+docker ps > /dev/null
+dockerState="$?"
+echo $dockerState
+done
 usermod -aG docker ubuntu
 apt install python3-pip -y
 pip3 install awscli
